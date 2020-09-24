1cdcfce28092b46b8c14dc67432884cdb3822312
re4388 <re4388@gmail.com>
2020-09-24 22:28:47 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+// import { DicomViewerModule } from 'dicomViewer';
+import { DicomViewerModule } from 'ng-dicomviewer';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    MatProgressSpinnerModule,
+    DicomViewerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
